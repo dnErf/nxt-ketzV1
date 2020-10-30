f57aa9b4ea1fc7833bc9b318e07910595bf1114f
dnErf <frend.fernando@live.com>
2020-10-29 18:40:34 -0700

    cadence events | jobs

--- cadence/src/index.ts
+++ cadence/src/index.ts
@@ -15,12 +15,13 @@ import { server } from './server';
     });
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+    new OrderCreatedListener(natsWrapper.client).listen();
     await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
      });
-    server.listen(9998, () => console.log('core at port 9998'))
+    server.listen(9997, () => console.log('cadence at port 9997'))
   }
   catch (err) {
     console.error(err);
